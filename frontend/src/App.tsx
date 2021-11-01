94958639f673bd3e6f1bc3dfd0ef31d5bec2b40e
kimsungho <kimsungho97@gmail.com>
2021-11-01 16:12:14 +0900

    [feat] Router설정 및 Recoil 설치 #5 - react-router-dom 설치

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
+import './reset.css';
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <RecoilRoot>
+            <BrowserRouter>
+                <Route
+                    exact
+                    path="/"
+                    component={() => <div className="App" />}
+                />
+            </BrowserRouter>
+        </RecoilRoot>
     );
 }
 
