bd10fdfbce58fa9aea0f1be9a7a526c76d3e2e17
kimsungho <kimsungho97@gmail.com>
2021-11-01 19:01:58 +0900

    [chore] database 연동 및 mysql2 설치 #4

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,6 +1,12 @@
 import { Router } from 'express';
+import { pool } from 'src/database/connection';
 
 // Export the base-router
 const baseRouter = Router();
 
+baseRouter.use('/test', async (req, res) => {
+    const [result]: any = await pool.query('SELECT * from temp');
+    console.log(JSON.parse(JSON.stringify(result)));
+    return res.send(JSON.parse(JSON.stringify(result)));
+});
 export default baseRouter;
