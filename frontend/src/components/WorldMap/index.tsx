729a8e2a8004cfd5c3e0241132fca009469ce1d9
jhLim97 <wnsgur2749@naver.com>
2021-11-04 11:22:26 +0900

    [feat] 월드 페이지에 필요한 Map(배경) 컴포넌트 구현

---
+++ frontend/src/components/WorldMap/index.tsx
@@ -0,0 +1,110 @@
+/* eslint-disable consistent-return */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable import/no-absolute-path */
+/* eslint-disable no-plusplus */
+import React, { useRef, useEffect, useState } from 'react';
+import styled from 'styled-components';
+
+const Canvas = styled.canvas`
+    margin-left: 0px;
+    overflow: hidden;
+`;
+
+interface ILayer {
+    data: number[];
+    height: number;
+    width: number;
+    imgSrc: string;
+    columnCount: number;
+}
+
+interface IProps {
+    data: ILayer[];
+}
+
+let cnt = 0;
+const backgroundImageList: HTMLImageElement[] = [];
+const WorldBackground = (props: IProps) => {
+    const layers = props.data;
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+    const [tileBackground, setTileBackground] = useState<HTMLImageElement[]>();
+    const commonWidth = layers[0].width;
+    const tileSize = 32;
+
+    let ctx: CanvasRenderingContext2D | null;
+    let sourceX = 0;
+    let sourceY = 0;
+
+    const spriteTileSize = 32;
+
+    const getIndex = (x: number, y: number) => {
+        return y * commonWidth + x;
+    };
+
+    useEffect(() => {
+        layers.forEach((layer) => {
+            const backgroundImg = new Image();
+            backgroundImg.src = layer.imgSrc;
+            backgroundImg.onload = () => {
+                cnt++;
+                backgroundImageList.push(backgroundImg);
+                if (cnt === layers.length) {
+                    setTileBackground(backgroundImageList);
+                }
+            };
+        });
+    }, []);
+
+    useEffect(() => {
+        const canvas: HTMLCanvasElement | null = canvasRef.current;
+        if (canvas === null) {
+            return;
+        }
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+
+        ctx = canvas.getContext('2d');
+        drawGame();
+        return () => drawGame();
+    }, [tileBackground]);
+
+    const drawGame = () => {
+        if (!ctx || !tileBackground) return;
+
+        layers.forEach((layer) => {
+            const indexOfLayers = layers.indexOf(layer);
+            drawBackground(layer, indexOfLayers);
+        });
+    };
+
+    const drawBackground = (layer: ILayer, indexOfLayers: number) => {
+        if (!ctx || !tileBackground) return;
+        for (let col = 0; col < layer.height; ++col) {
+            for (let row = 0; row < layer.width; ++row) {
+                let tileVal = layer.data[getIndex(row, col)];
+                if (tileVal !== 0) {
+                    tileVal -= 1;
+                    sourceY =
+                        Math.floor(tileVal / layer.columnCount) *
+                        spriteTileSize;
+                    sourceX = (tileVal % layer.columnCount) * spriteTileSize;
+                    ctx.drawImage(
+                        backgroundImageList[indexOfLayers],
+                        sourceX,
+                        sourceY,
+                        spriteTileSize,
+                        spriteTileSize,
+                        row * tileSize,
+                        col * tileSize,
+                        tileSize,
+                        tileSize,
+                    );
+                }
+            }
+        }
+    };
+
+    return <Canvas id="canvas" ref={canvasRef} />;
+};
+
+export default WorldBackground;
