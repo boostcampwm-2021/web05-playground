d4dd11550037c2a81b7dac6382500b3c0a9f8a65
minjae <minjaec023@gmail.com>
2021-11-04 18:55:14 +0900

    [refactor] 함수 인자 및 반환 타입 변경, 핸들러 분리

--- backend/src/socket/socket.ts
+++ backend/src/socket/socket.ts
@@ -1,46 +1,46 @@
 import express from 'express';
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 import { getUserInfo, addUser, deleteUser } from './socket.user';
 import { IUser } from '../database/entities/User';
+import * as http from 'http';
 
-export default class Socket {
+class MySocket extends Socket {
+    public uid?: number;
+}
+
+export default class RoomSocket {
     public app: express.Application;
     public port: number;
-    public server: any;
-    public io: any;
+    public server?: http.Server;
+    public io!: Server;
     public userList: Array<IUser> = [];
 
     constructor(port: number) {
         this.app = express();
         this.port = port;
-    }
-    
-    public listen() {
         this.server = this.app.listen(this.port, () => {
             console.log(`Socket listening on the port ${this.port}`);
         });
         this.io = new Server(this.server, {cors: {origin: '*'}});
     }
-
+    
     public connect() {
-        this.io.on('connection', (socket: any) => {
+        this.io.on('connection', (socket: MySocket) => {
             console.log(`${socket.id} 연결되었습니다.`);
-
-            socket.on('user', async (data:string) => {
-                const user = await getUserInfo(data);
-                socket.email = user.email;
-
-                addUser(user, this.userList);
-
-                console.log(this.userList);
-                socket.broadcast.emit('user', this.userList);
-            })
-
-            socket.on('disconnect', () => {
-                this.userList = deleteUser(socket.email, this.userList);
-                console.log(this.userList);
-                console.log(`${socket.email} 끊어졌습니다.`);
-            })
+            socket.on('user', (data: string) => this.addUserHandler(data, socket));
+            socket.on('disconnect', () => this.deleteUserHandler(socket));
         });
+    }
+
+    async addUserHandler(data: string, socket: MySocket) {
+        const user = await getUserInfo(data);
+        socket.uid = user.id;
+        addUser(user, this.userList);
+        this.io.emit('user', this.userList);
+    }
+
+    deleteUserHandler(socket: MySocket) {
+        if(socket.uid !== undefined) this.userList = deleteUser(socket.uid, this.userList);
+        console.log(`${socket.id} 끊어졌습니다.`);
     }
 }
--- backend/src/socket/socket.user.ts
+++ backend/src/socket/socket.user.ts
@@ -1,16 +1,19 @@
 import { getUser } from '../database/service/user.service';
 import { IUser } from '../database/entities/User';
+import { STATUS_CODE } from '@shared/db.receiver';
+import { userListError } from '@shared/constants';
 
 export const getUserInfo = async (email: string): Promise<IUser> => {
     const userInfo = await getUser(email);
-    return userInfo;
+    if(userInfo.user === undefined) throw new Error(userListError);
+    if(userInfo.status === STATUS_CODE.FAIL) throw new Error(userListError);
+    return userInfo.user; 
 }
-
 export const addUser = (user: IUser, userList: Array<IUser>): void => {
     userList.push(user);
 }
 
-export const deleteUser = (email: string, userList: Array<IUser>): Array<IUser> => {
-    const newUserList = userList.filter((user) => user.email !== email);
+export const deleteUser = (uid: number, userList: Array<IUser>): Array<IUser> => {
+    const newUserList = userList.filter((user) => user.id !== uid);
     return newUserList;
 }
