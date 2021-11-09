bc2f77edd2a14bffc0dc0df7c6dac5ebcc042479
minseok <kms9887@naver.com>
2021-11-09 15:09:57 +0900

    [feat] 모달 상태에 따른 모달창 여부 구현 현재 모달의 상태를 확인 후 모달을 띄울지 말지 결정하는 코드 구현

--- frontend/src/pages/World/index.tsx
+++ frontend/src/pages/World/index.tsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { useRecoilState } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import { RouteComponentProps } from 'react-router';
 
 import currentWorldState from '../../store/currentWorldState';
+import currentModalState from '../../store/currentModalState';
 
 import WorldBackground from '../../components/WorldMap';
 import NavigationBar from '../../components/NavigationBar';
+import Modal from '../../components/Modal';
 
 import worldPark from '../../map-files/world-park.json';
 import worldWinter from '../../map-files/world-winter.json';
@@ -17,6 +19,8 @@ const worldsInfo: any = {
 
 const World = (props: RouteComponentProps) => {
     const [currentWorld, setCurrentWorld] = useRecoilState(currentWorldState);
+    const currentModal = useRecoilValue(currentModalState);
+
     if (currentWorld.name === 'default') {
         props.history.push('/selectworld');
         return <></>;
@@ -39,6 +43,7 @@ const World = (props: RouteComponentProps) => {
     return (
         <>
             <WorldBackground data={mapLayers} />
+            {currentModal !== 'none' ? <Modal /> : <div>{currentModal}</div>}
             <NavigationBar />
         </>
     );
