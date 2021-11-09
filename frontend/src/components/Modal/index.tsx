03e6f19abf4e8b546d8fc0cd3eb4c94cd5da1160
jhLim97 <wnsgur2749@naver.com>
2021-11-09 16:05:16 +0900

    [refactor] 모달창 < 네비게이션바 z-index 설정 및 스타일 속성 컴포넌트 파일 하단으로 이동

--- frontend/src/components/Modal/index.tsx
+++ frontend/src/components/Modal/index.tsx
@@ -5,21 +5,6 @@ import styled from 'styled-components';
 
 import currentModalState from '../../store/currentModalState';
 
-const ModalDiv = styled.div`
-    height: 100vh;
-    width: 400px;
-    background: #c4c4c4;
-    position: fixed;
-    right: 0;
-    top: 0px;
-`;
-
-const BackBtn = styled.img`
-    padding: 10px;
-    width: 30px;
-    height: 30px;
-`;
-
 const Modal = () => {
     const [currentModal, setCurrentModal] = useRecoilState(currentModalState);
     return (
@@ -31,3 +16,20 @@ const Modal = () => {
 };
 
 export default Modal;
+
+const ModalDiv = styled.div`
+    position: fixed;
+    z-index: 2;
+
+    height: 100vh;
+    width: 400px;
+    background: #c4c4c4;
+    right: 0;
+    top: 0px;
+`;
+
+const BackBtn = styled.img`
+    padding: 10px;
+    width: 30px;
+    height: 30px;
+`;
