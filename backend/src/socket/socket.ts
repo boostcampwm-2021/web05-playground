d0de55a94342881e7bd6421472db74d8a9467aa9
kimsungho <kimsungho97@gmail.com>
2021-11-10 13:27:34 +0900

    [feat] 백엔드 캐릭터이동 관련 소켓 동작 구현 #31

--- backend/src/socket/socket.ts
+++ backend/src/socket/socket.ts
@@ -1,6 +1,12 @@
 import express from 'express';
 import { Server, Socket } from 'socket.io';
-import { getUserInfo, addUser, deleteUser } from './socket.user';
+import {
+    UserMove,
+    getUserInfo,
+    addUser,
+    deleteUser,
+    moveUser,
+} from './socket.user';
 import { IUser } from '../database/entities/User';
 import * as http from 'http';
 
@@ -13,7 +19,7 @@ export default class RoomSocket {
     public port: number;
     public server?: http.Server;
     public io!: Server;
-    public userList: Array<IUser> = [];
+    public userMap: Map<number, IUser> = new Map();
 
     constructor(port: number) {
         this.app = express();
@@ -21,13 +27,18 @@ export default class RoomSocket {
         this.server = this.app.listen(this.port, () => {
             console.log(`Socket listening on the port ${this.port}`);
         });
-        this.io = new Server(this.server, {cors: {origin: '*'}});
+        this.io = new Server(this.server, { cors: { origin: '*' } });
     }
-    
+
     public connect() {
         this.io.on('connection', (socket: MySocket) => {
             console.log(`${socket.id} 연결되었습니다.`);
-            socket.on('user', (data: string) => this.addUserHandler(data, socket));
+            socket.on('user', (data: string) =>
+                this.addUserHandler(data, socket),
+            );
+            socket.on('move', (data: UserMove) =>
+                this.moveHandler(data, socket),
+            );
             socket.on('disconnect', () => this.deleteUserHandler(socket));
         });
     }
@@ -35,12 +46,17 @@ export default class RoomSocket {
     async addUserHandler(data: string, socket: MySocket) {
         const user = await getUserInfo(data);
         socket.uid = user.id;
-        addUser(user, this.userList);
-        this.io.emit('user', this.userList);
+        addUser(user, this.userMap);
+        this.io.emit('user', this.userMap);
+    }
+
+    async moveHandler(data: UserMove, socket: MySocket) {
+        moveUser(data, this.userMap);
+        socket.broadcast.emit('move', data);
     }
 
     deleteUserHandler(socket: MySocket) {
-        if(socket.uid !== undefined) this.userList = deleteUser(socket.uid, this.userList);
+        if (socket.uid !== undefined) deleteUser(socket.uid, this.userMap);
         console.log(`${socket.id} 끊어졌습니다.`);
     }
 }
--- backend/src/socket/socket.user.ts
+++ backend/src/socket/socket.user.ts
@@ -3,17 +3,49 @@ import { IUser } from '../database/entities/User';
 import { STATUS_CODE } from '@shared/db.receiver';
 import { userListError } from '@shared/constants';
 
-export const getUserInfo = async (email: string): Promise<IUser> => {
-    const userInfo = await getUser(email);
-    if(userInfo.user === undefined) throw new Error(userListError);
-    if(userInfo.status === STATUS_CODE.FAIL) throw new Error(userListError);
-    return userInfo.user; 
-}
-export const addUser = (user: IUser, userList: Array<IUser>): void => {
-    userList.push(user);
+export enum Direction {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT,
 }
 
-export const deleteUser = (uid: number, userList: Array<IUser>): Array<IUser> => {
-    const newUserList = userList.filter((user) => user.id !== uid);
-    return newUserList;
+export interface UserMove {
+    id: number;
+    email: string;
+    direction: Direction;
 }
+
+export const getUserInfo = async (email: string): Promise<IUser> => {
+    const userInfo = await getUser(email);
+    if (userInfo.user === undefined) throw new Error(userListError);
+    if (userInfo.status === STATUS_CODE.FAIL) throw new Error(userListError);
+    return userInfo.user;
+};
+export const addUser = (user: IUser, userMap: Map<number, IUser>): void => {
+    userMap.set(user.id, user);
+};
+
+export const deleteUser = (uid: number, userMap: Map<number, IUser>) => {
+    userMap.delete(uid);
+};
+
+export const moveUser = (data: UserMove, userMap: Map<number, IUser>) => {
+    const userInfo = userMap.get(data.id);
+    if (userInfo === undefined) throw new Error(userListError);
+
+    switch (data.direction) {
+        case Direction.UP:
+            userInfo.y += 1;
+            break;
+        case Direction.DOWN:
+            userInfo.y -= 1;
+            break;
+        case Direction.LEFT:
+            userInfo.x -= 1;
+            break;
+        case Direction.RIGHT:
+            userInfo.x += 1;
+            break;
+    }
+};
