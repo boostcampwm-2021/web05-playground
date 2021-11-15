415395ffbb734b57f52f8f3ecd5c5b9d6d55f06b
minseok <kms9887@naver.com>
2021-11-15 15:53:51 +0900

    [feat] 오브젝트 선택하는 모달창 구현

---
+++ frontend/src/components/Modal/BuildObject/index.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { useSetRecoilState, useRecoilValue } from 'recoil';
+
+import styled from 'styled-components';
+import buildObjectState from '../../../store/buildObjectState';
+import objectUrls from '../../../store/objectUrlState';
+
+interface customEventTarget extends EventTarget {
+    src: string;
+}
+
+interface customMouseEvent extends React.MouseEvent<HTMLButtonElement, MouseEvent> {
+    target: customEventTarget;
+}
+
+const BuildObject = () => {
+    const setBuildObject = useSetRecoilState(buildObjectState);
+    const objectUrl = useRecoilValue(objectUrls);
+
+    const selectObject = (e: customMouseEvent) => {
+        const selectedObjectInfo = {
+            objectSrc: e.target.src,
+            locationX: -1,
+            locationY: -1,
+            isLocated: false,
+            isObject: true,
+        };
+        setBuildObject(selectedObjectInfo);
+    };
+
+    return (
+        <ImgContainer>
+            {objectUrl.map((url) => {
+                return (
+                    <ImgBtn key={url.url} type="button" onMouseDown={selectObject}>
+                        <InnerImg src={url.url} alt="빌드 가능한 오브젝트 이미지" />
+                    </ImgBtn>
+                );
+            })}
+        </ImgContainer>
+    );
+};
+
+export default BuildObject;
+
+const ImgContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-around;
+`;
+
+const ImgBtn = styled.button`
+    margin: 10px;
+    background-color: #c4c4c4;
+    border: 0;
+`;
+
+const InnerImg = styled.img`
+    width: 150px;
+    height: 150px;
+`;
