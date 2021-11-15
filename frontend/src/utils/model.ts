3db0b7bca6b4357bc6bcd7097cb8cecfa44b4616
jhLim97 <wnsgur2749@naver.com>
2021-11-15 18:20:08 +0900

    [feat] 채팅메세지, 모달 활성화를 위한 인터페이스 구현

--- frontend/src/utils/model.ts
+++ frontend/src/utils/model.ts
@@ -33,3 +33,16 @@ export interface IProps {
     layers: ILayer[];
     buildingList: IBuilding[];
 }
+
+export interface MessageInfo {
+    id: string;
+    message: string;
+}
+
+export interface MessageInfos {
+    messageInfos: MessageInfo[];
+}
+
+export interface ActiveModal {
+    active: boolean;
+}
