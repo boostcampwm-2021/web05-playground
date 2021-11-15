74e43d713e66df69a6d331ff25bf7483be36a624
minseok <kms9887@naver.com>
2021-11-15 15:25:04 +0900

    [feat] 오브젝트 빌드 서비스 추가 오브젝트 추가하는 기능 구현

--- backend/src/database/service/object.service.ts
+++ backend/src/database/service/object.service.ts
@@ -1,6 +1,11 @@
-import { objectListError, objectUrlError } from '@shared/constants';
+import {
+    addObjectError,
+    objectListError,
+    objectUrlError,
+} from '@shared/constants';
 import { Receiver, STATUS_CODE } from '@shared/db.receiver';
 import { pool1, pool2 } from '../connection';
+import { IObject } from '../entities/Object';
 
 export const getObjectList = async (): Promise<Receiver> => {
     const result: Receiver = {
@@ -33,6 +38,33 @@ export const getObjectListByBid = async (bid: number): Promise<Receiver> => {
     } catch (err) {
         result.status = STATUS_CODE.FAIL;
         result.err = objectListError;
+        return result;
+    }
+};
+
+export const addObject = async (data: IObject): Promise<Receiver> => {
+    const result: Receiver = {
+        status: STATUS_CODE.SUCCESS,
+    };
+
+    try {
+        const sql = `INSERT INTO object(id, bid, x, y, image_url, file_url) VALUES (?, ?, ?, ?, ?, ?)`;
+        const values = [
+            data.id,
+            data.bid,
+            data.x,
+            data.y,
+            data.imageUrl,
+            data.fileUrl,
+        ];
+        await pool2.query(sql, values);
+
+        result.addedObject = data;
+        return result;
+    } catch (err) {
+        console.log(err);
+        result.status = STATUS_CODE.FAIL;
+        result.err = addObjectError;
         return result;
     }
 };
