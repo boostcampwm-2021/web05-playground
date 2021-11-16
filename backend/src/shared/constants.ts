4ca7dea7932a33bca5369f768a78c38447ecb002
kimsungho <kimsungho97@gmail.com>
2021-11-16 18:29:30 +0900

    [feat] 월드 addUser 에러 추가

--- backend/src/shared/constants.ts
+++ backend/src/shared/constants.ts
@@ -11,3 +11,4 @@ export const buildingUrlError = 'Fail to get Building Url List';
 
 export const characterError = 'Fail to get Charater List';
 export const setUserError = 'Fail to set User Info';
+export const addUserError = 'Fail to add User Info';
