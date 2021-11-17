02474414f654e44012a84b4f4225b2358c6d1036
kimsungho <kimsungho97@gmail.com>
2021-11-17 15:19:13 +0900

    [refactor] backend objectResolver 주석 제거

--- backend/src/graphql/resolver/objectResolver.ts
+++ backend/src/graphql/resolver/objectResolver.ts
@@ -7,24 +7,6 @@ import {
 
 const objectResolver: IResolvers = {
     Query: {
-        /*async objectList(): Promise<Array<object>> {
-            const result = await getObjectList();
-            if (
-                result.status == STATUS_CODE.SUCCESS &&
-                result.objectArr !== undefined
-            )
-                return result.objectArr;
-            else throw new Error(result.err);
-        },
-        async objectListByBid(_: void, bid: number): Promise<Array<object>> {
-            const result = await getObjectListByBid(bid);
-            if (
-                result.status == STATUS_CODE.SUCCESS &&
-                result.objectArr !== undefined
-            )
-                return result.objectArr;
-            else throw new Error(result.err);
-        },*/
         async objectUrl(): Promise<Array<object>> {
             const result = await getObjectUrl();
             if (
