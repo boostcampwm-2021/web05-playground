f0f3b4238933054ac23d48483864c0307c13cd28
kimsungho <kimsungho97@gmail.com>
2021-11-17 17:26:58 +0900

    [feat] 워커 서버 분리작업 진행 #46

---
+++ worker-server/src/database/service/building.service.ts
@@ -0,0 +1,54 @@
+import {
+    buildingListError,
+    addBuildingError,
+    buildingUrlError,
+} from '@shared/constants';
+import { Receiver, STATUS_CODE } from '@shared/db.receiver';
+import { pool2 } from '../connection';
+import { IBuilding } from '../entities/Building';
+
+export const getBuildingList = async (): Promise<Receiver> => {
+    const result: Receiver = {
+        status: STATUS_CODE.SUCCESS,
+    };
+
+    try {
+        const sql = `SELECT id, x, y, uid, description, scope, password, image_url as imageUrl FROM building`;
+        const [buildings] = await pool2.query(sql);
+
+        result.buildingArr = JSON.parse(JSON.stringify(buildings));
+        return result;
+    } catch (err) {
+        result.status = STATUS_CODE.FAIL;
+        result.err = buildingListError;
+        return result;
+    }
+};
+
+export const addBuilding = async (data: IBuilding): Promise<Receiver> => {
+    const result: Receiver = {
+        status: STATUS_CODE.SUCCESS,
+    };
+
+    try {
+        const sql = `INSERT INTO building(x, y, uid, description, scope, password, image_url) VALUES (?, ?, ?, ?, ?, ?, ?)`;
+        const values = [
+            data.x,
+            data.y,
+            data.uid,
+            data.description,
+            data.scope,
+            data.password,
+            data.imageUrl,
+        ];
+        await pool2.query(sql, values);
+
+        result.addedBuilding = data;
+        return result;
+    } catch (err) {
+        console.log(err);
+        result.status = STATUS_CODE.FAIL;
+        result.err = addBuildingError;
+        return result;
+    }
+};
---
+++ worker-server/src/shared/constants.ts
@@ -0,0 +1,18 @@
+//database/service Error
+export const worldListError = 'Fail to get World List';
+
+export const objectListError = 'Fail to get Object List';
+export const addObjectError = 'Fail to insert Object';
+
+export const objectUrlError = 'Fail to get Object Url List';
+
+export const userListError = 'Fail to get User List';
+
+export const buildingListError = 'Fail to get Building List';
+export const addBuildingError = 'Fail to insert Building';
+
+export const buildingUrlError = 'Fail to get Building Url List';
+
+export const characterError = 'Fail to get Charater List';
+export const setUserError = 'Fail to set User Info';
+export const addUserError = 'Fail to add User Info';
