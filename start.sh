380b4eaa68a172fdf3dc2f3b2edd6c610ad09f5c
kimsungho <kimsungho97@gmail.com>
2021-11-22 16:58:03 +0900

    [ci] Jenkins가 실행할 shell script 파일 추가

---
+++ start.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# 실행중인 도커 컨테이너 중지
+echo "stop container"
+docker stop $(docker ps -a -q)
+
+# 도커 컨테이너 삭제
+echo "delete container"
+docker rm $(docker ps -a -q)
+
+mkdir ~/web05-playground/worker-server/src/pre-start/env
+mv /root/env.txt ~/web05-playground/worker-server/src/pre-start/env
+
+# 노드 서버 이미지 빌드
+echo "image build"
+docker build ./worker-server --tag kimsungho97/worker-server
+
+# db 서버 이미지 빌드
+echo "db build"
+docker build ../ --tag kimsungho97/mysql:5.7
+
+# 첫 번째 월드 컨테이너 실행
+echo "first container run"
+PORT=8000 docker-compose -f ./worker-server/docker-compose.yml up -d
+
+# 두 번째 월드 컨테이너 실행
+echo "second container run"
+PORT=9000 docker-compose -f ./worker-server/docker-compose.yml -p worker2 up -d
