e74f07789cbeabd0f21ff0ae737feca3ccab2131
minseok <kms9887@naver.com>
2021-11-27 18:07:05 +0900

    [feat] 건물 진입 시 정원 체크 로직 구현

--- frontend/src/components/Modal/BuildingInfo/index.tsx
+++ frontend/src/components/Modal/BuildingInfo/index.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-alert */
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRecoilState } from 'recoil';
 
 import styled from 'styled-components';
@@ -12,6 +12,20 @@ const BuildingInfo = () => {
     const [buildingInfo, setBuildingInfo] = useRecoilState(buildingInfoState);
     const [password, setPassword] = useState('');
     const [isInbuilding, setIsInBuilding] = useRecoilState(isInBuildingState);
+
+    useEffect(() => {
+        socketClient.on('checkCapacity', (isFull: boolean) => {
+            if (isFull) {
+                alert('방이 꽉찼습니다!');
+                return;
+            }
+            setIsInBuilding(buildingInfo.id);
+            cancle();
+        });
+        return () => {
+            socketClient.removeListener('checkCapacity');
+        };
+    }, [socketClient]);
 
     const cancle = () => {
         setBuildingInfo({
@@ -34,8 +48,7 @@ const BuildingInfo = () => {
     };
 
     const joinRoom = () => {
-        setIsInBuilding(buildingInfo.id);
-        cancle();
+        socketClient.emit('checkCapacity', buildingInfo.id.toString(10));
     };
 
     const changed = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
