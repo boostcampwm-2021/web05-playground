57c04e636ed9e8c34bfebc9b11e4ad1e74eb47e5
jhLim97 <wnsgur2749@naver.com>
2021-11-29 17:01:51 +0900

    [chore] 이미지 fetch url 수정

--- frontend/src/workers/buildingLoad.ts
+++ frontend/src/workers/buildingLoad.ts
@@ -1,22 +1,7 @@
 /* eslint-disable no-return-await */
-const imageMap = new Map();
-imageMap.set(
-    'https://cdn.discordapp.com/attachments/905266906259419141/907927778584829982/pngwing.com.png',
-    'https://jwwhhmlzueei8656692.cdn.ntruss.com/building1.png',
-);
-imageMap.set(
-    'https://media.discordapp.net/attachments/905266906259419141/908349179703726080/pngwing.com_6.png',
-    'https://jwwhhmlzueei8656692.cdn.ntruss.com/building3.png',
-);
-imageMap.set(
-    'https://media.discordapp.net/attachments/905266906259419141/909676468689326080/premium-icon-whiteboard-2293918_1.png',
-    'https://jwwhhmlzueei8656692.cdn.ntruss.com/object1.png',
-);
 
-// 일단 지금 cors 설정때문에 임시로 데이터 변경
 const getImageBitMap = async (imgUrl: string) => {
-    const convertImgUrl = imageMap.get(imgUrl);
-    const response = await fetch(convertImgUrl, {
+    const response = await fetch(imgUrl, {
         mode: 'cors',
         credentials: 'include',
     });
--- frontend/src/workers/characterLoad.ts
+++ frontend/src/workers/characterLoad.ts
@@ -1,9 +1,4 @@
 /* eslint-disable no-return-await */
-const imageMap = new Map();
-imageMap.set('/assets/character1.png', 'https://jwwhhmlzueei8656692.cdn.ntruss.com/character1.png');
-imageMap.set('/assets/character2.png', 'https://jwwhhmlzueei8656692.cdn.ntruss.com/character2.png');
-imageMap.set('/assets/character3.png', 'https://jwwhhmlzueei8656692.cdn.ntruss.com/character3.png');
-
 const buildingImageCache = new Map();
 
 const makeImageBitMapList = async (characters: any) => {
@@ -15,12 +10,10 @@ const makeImageBitMapList = async (characters: any) => {
 };
 
 const getImageBitMap = async (imgUrl: string) => {
-    const convertImgUrl = imageMap.get(imgUrl);
-
     const cachedImg = buildingImageCache.get(imgUrl);
     if (cachedImg) return cachedImg;
 
-    const response = await fetch(convertImgUrl);
+    const response = await fetch(imgUrl);
     const blob = await response.blob();
     const imageBitmap = await createImageBitmap(blob);
     buildingImageCache.set(imgUrl, imageBitmap);
