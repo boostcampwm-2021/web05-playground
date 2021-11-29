dfaf44222b59bf52aee93f5cb39b5ab48ff51e87
jhLim97 <wnsgur2749@naver.com>
2021-11-29 16:39:24 +0900

    [feat] 로그인 잘못 변경된 로직 재수정

--- frontend/src/pages/SelectWorld/index.tsx
+++ frontend/src/pages/SelectWorld/index.tsx
@@ -17,28 +17,16 @@ const SelectWorld = (props: RouteComponentProps) => {
     const code = searchParams.get('code');
 
     useEffect(() => {
-        // if (user.id !== 1) return;
-        // if (code) {
-        //     const Login = async (code: string) => {
-        //         const userInfo = (await fetchUser({ variables: { code } })).data.user;
-        //         setUser(userInfo);
-        //     };
-        //     Login(code);
-        //     return;
-        // }
-        // props.history.push('/login');
-        const Login = async () => {
-            // const userInfo = (await fetchUser({ variables: { code } })).data.user;
-            setUser({
-                id: 1,
-                nickname: 'minjae',
-                email: 'minjaec023@gmail.com',
-                x: 10,
-                y: 10,
-                imageUrl: '/assets/character1.png',
-            });
-        };
-        Login();
+        if (user.id !== 1) return;
+        if (code) {
+            const Login = async (code: string) => {
+                const userInfo = (await fetchUser({ variables: { code } })).data.user;
+                setUser(userInfo);
+            };
+            Login(code);
+            return;
+        }
+        props.history.push('/login');
     }, []);
 
     const redirectSetting = (event: React.MouseEvent) => {
